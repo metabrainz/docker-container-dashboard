25cc0eaf31b3c8b86372e3dee665bd08e88edc49
akshaaatt <tiwariakshat03@gmail.com>
2022-05-18 04:07:51 +0530

    Fix css on gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  basePath: '/docker-container-dashboard',
+  assetPrefix: '/docker-container-dashboard/'
 }
 
 module.exports = nextConfig
